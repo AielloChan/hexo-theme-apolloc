7ca50cbfd33f082581dbe89d3e89c72bd3bbd27c
Aiello Chan <aiello.chan@gmail.com>
2021-03-15 16:49:37 +0800

    update(dependencies): Replace node-sass with dart-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var { task, src, dest, series, watch } = require('gulp');
+var sass = require('gulp-dart-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-// 一次性编译 Sass 
-gulp.task('sass', function() {
-    return gulp.src('./source/scss/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
+// 一次性编译 Sass
+task('sass', function () {
+    return src('./source/scss/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(autoprefixer())
-        .pipe(gulp.dest('./source/css'));
+        .pipe(dest('./source/css'));
 });
 
 // 实时编译
-gulp.task('default', ['sass'], function() {
-    gulp.watch('./source/scss/_partial/*.scss', ['sass']);
-    gulp.watch('./source/scss/*.scss', ['sass']);
+task('default', series('sass'), function () {
+    watch('./source/scss/_partial/*.scss', ['sass']);
+    watch('./source/scss/*.scss', ['sass']);
 });
